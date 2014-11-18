f485c58351b15b5affa1026b2bfacd49d419ef7a
Aishvarya Krishnan <aishvarya.krishnan@gmail.com>
2014-11-17 20:26:09 -0500

    test

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,48 +3,45 @@ angular.module('starter.controllers', [])
     .controller('DashCtrl', function($scope, user) {
     })
     .controller('MapCtrl', function($scope, $cordovaGeolocation) {
-        console.log("init map");
-        $scope.msg = "";
-        $scope.coords = [0,0];
+	$scope.coords = [0,0];
         $scope.mapVisible = true;
+        var onSuccess = function(position) {
+                var myLat = position.coords.latitude;
+                var myLong = position.coords.longitude;
 
-        var init = function () {
-            var mapOptions = {};
+                //MAP
+                var mapOptions = {
+                    center: new google.maps.LatLng(myLat, myLong),
+                    zoom: 14,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                };
+
             var map = new google.maps.Map(document.getElementById("map"),
-                mapOptions);
+                                              mapOptions);
+ $scope.map = map;
+                var marker = new google.maps.Marker({
+                                                    position: new google.maps.LatLng(myLat, myLong),
+                                                    map: map,
+                                                    title:"Hello World!"
+                                                    });
 
-            $scope.map = map;
+            };
 
-            // get coords
-            $cordovaGeolocation.getCurrentPosition().then(function(position) {
-			$scope.loc = position
-                // Position here: position.coords.latitude, position.coords.longitude
-                console.log("setting map");
-                $scope.msg = position.coords.latitude + ":" + position.coords.longitude;
-                $scope.updateCenter(parseFloat(position.coords.latitude), parseFloat(position.coords.longitude));
-            }, function(err) {
-                $scope.msg = "unable to determine location";
-            });
-        };
-		
-        $scope.updateCenter = function(lat, lng) {
-            /*var mapOptions = {
-                center: new google.maps.LatLng(0,0),
-                zoom: 16,
-                mapTypeId: google.maps.MapTypeId.ROADMAP
-            };*/
-		
-			
-			
-            $scope.map.setCenter(new google.maps.LatLng(lat, lng));
-            $scope.map.setZoom(16);
-            $scope.centerLat = lat;
-            $scope.centerLng = lng;
-            $scope.mapVisible =true;
-        };
+            // onError Callback receives a PositionError object
+            //
+            function onError(error) {
+                alert('code: '    + error.code    + '\n' +
+                      'message: ' + error.message + '\n');
+            }
+
+            navigator.geolocation.getCurrentPosition(onSuccess, onError);
+        
 
 
-        init();
+           
+
+     
+
     })
 
 
