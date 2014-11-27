909e83ba5d443011331017dadc2d18454f27b4cd
Aishvarya Krishnan <aishvarya.krishnan@gmail.com>
2014-11-27 03:37:18 -0500

    test

--- www/js/services.js
+++ www/js/services.js
@@ -1,8 +1,26 @@
-angular.module('starter.services', ['ionic', 'ngResource'])
+angular.module('starter.services', [])
 
 /**
  * A simple example service that returns some data.
  */
-.factory('AddWalker', function($resource) {
-  return $resource('http://stormy-badlands-7597.herokuapp.com/mas/api/v1.0/tasks/addwalker');
+.factory('Friends', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var friends = [
+    { id: 0, name: 'Scruff McGruff' },
+    { id: 1, name: 'G.I. Joe' },
+    { id: 2, name: 'Miss Frizzle' },
+    { id: 3, name: 'Ash Ketchum' }
+  ];
+
+  return {
+    all: function() {
+      return friends;
+    },
+    get: function(friendId) {
+      // Simple index lookup
+      return friends[friendId];
+    }
+  }
 });
