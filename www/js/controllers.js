1446000544a940092737f808af8a1e6dd3631064
Aishvarya Krishnan <aishvarya.krishnan@gmail.com>
2014-11-27 16:51:51 -0500

    test

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -72,6 +72,7 @@ google.maps.event.addListener(marker, "dragend", function() {
     .controller('FriendsCtrl', function($scope, $http) {
 	$http.get('https://stormy-badlands-7597.herokuapp.com/mas/api/v1.0/tasks/getwalkers').success(function(data) {
 	var d = data.walkers;
+	$scope.friends = [];
 	for(var i in d){
 		$scope.friends.push(d[i]);
      }
