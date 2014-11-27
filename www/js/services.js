0da113b0650a4df7a5e5be9b30f31f2ea669a6e6
Aishvarya Krishnan <aishvarya.krishnan@gmail.com>
2014-11-27 03:26:34 -0500

    test

--- www/js/services.js
+++ www/js/services.js
@@ -3,6 +3,6 @@ angular.module('starter.services', ['ionic', 'ngResource'])
 /**
  * A simple example service that returns some data.
  */
-..factory('AddWalker', function($resource) {
+.factory('AddWalker', function($resource) {
   return $resource('http://stormy-badlands-7597.herokuapp.com/mas/api/v1.0/tasks/addwalker');
 });
