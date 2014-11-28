67494bba4aa14dc2437f7e510fc24e15997f7f54
Aishvarya Krishnan <aishvarya.krishnan@gmail.com>
2014-11-27 22:01:00 -0500

    test

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', ['angular.filter','ngAnimate','require','underscore'])
+angular.module('starter.controllers', ['angular.filter','ngAnimate','underscore'])
 
     .controller('DashCtrl', function($scope) {
     })
