7470e8e32a5ff0778ec74cf69053df4ef8e2b59f
Aishvarya Krishnan <aishvarya.krishnan@gmail.com>
2014-11-29 00:25:35 -0500

    test

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,10 +2,10 @@ angular.module('starter.controllers', ['angular.filter','ngAnimate','ngUnderscor
 
     .controller('DashCtrl', function($scope, $rootScope) {
 	$scope.$root.tabsHidden = "tabs-hide";
-	$rootScope.name = $scope.user.name;
+	$rootScope.name = document.getElementById('name').value;
     })
     .controller('MapCtrl', function($scope, $cordovaGeolocation, $http,$window,$rootScope) {
-	$scope.user.name = $rootScope.name;
+	document.getElementById('name').value = $rootScope.name;
 	$scope.$root.tabsHidden = "tabs-hide";
 	$http.get('https://stormy-badlands-7597.herokuapp.com/mas/api/v1.0/tasks/getlocations').success(function(data) {
 	$scope.clientSideList = data.locations;
