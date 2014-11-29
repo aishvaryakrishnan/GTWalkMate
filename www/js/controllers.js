f12da16ef2d79e83415fff91d9e8268959fddb3d
Aishvarya Krishnan <aishvarya.krishnan@gmail.com>
2014-11-29 00:08:20 -0500

    test

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -68,7 +68,7 @@ google.maps.event.addListener(marker, "dragend", function() {
             navigator.geolocation.getCurrentPosition(onSuccess, onError);
         
 		 $scope.newWalker = function() {
-	   $window.location.href = #/tab/friends
+	   $window.location.href = '#/tab/friends';
     };	
     })
 
